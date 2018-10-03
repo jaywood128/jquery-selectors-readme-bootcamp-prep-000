9d8fa07af21a184a2407432a2117e1a2fbd867e2
Johnathon Wood <jaywood128@gmail.com>
2018-10-03 12:33:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,7 @@ function paragraphSelector() {
  var paragraph = document.getElementsByTagName('p')
   return paragraph
 }// declare your functions here...
+function lastImageSelector() {
+  var lastImage = $('img:last')
+  return lastImage
+}
