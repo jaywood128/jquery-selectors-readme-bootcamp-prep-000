34314fc5ba6cf8cc2e34c5fc45cefa4b4ce83a86
Johnathon Wood <jaywood128@gmail.com>
2018-10-03 12:57:45 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,5 +4,9 @@ function paragraphSelector() {
 }// declare your functions here...
 function lastImageSelector() {
   var lastImage = $('img:last')
-  return lastImage
+    return lastImage
+}
+function ninjaBabySelector() {
+   var ninjaBaby = ${'div img:first-child'}
+     return ninjaBaby
 }
